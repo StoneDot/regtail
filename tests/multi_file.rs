562a9a9a19dc93aee3147652d6221865f22cb7a9
Hiroaki Goto <1642211+StoneDot@users.noreply.github.com>
2019-02-02 14:32:24 +0900

    Fix multi file handling and add test

---
+++ tests/multi_file.rs
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 StoneDot (Hiroaki Goto)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+use std::process::Command;
+use std::thread;
+use std::time::Duration;
+use thread::sleep;
+
+use utils::RunningCommand;
+use utils::WorkingDir;
+use utils::KillStatus;
+
+#[macro_use]
+mod macros;
+mod utils;
+
+const WAIT_TIME: Duration = Duration::from_millis(50);
+
+test!(multi_file_with_eol, |dir: WorkingDir, mut cmd: Command| {
+    let mut child = RunningCommand::create(cmd.arg(dir.path_arg()).spawn().unwrap());
+    sleep(WAIT_TIME);
+    dir.put_file("file1", "test1!\n");
+    sleep(WAIT_TIME);
+    dir.put_file("file2", "test2!\n");
+    sleep(WAIT_TIME);
+    let result = child.exit();
+    assert_eq!(result, KillStatus::Killed);
+    let output = child.output();
+    assert!(output.contains(" <==\ntest1!\n\n==>"));
+    assert!(output.contains(" <==\ntest2!\n"));
+});
