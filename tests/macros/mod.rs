3368caeb80c2c01fbe7efe9adca769b314b004ca
Hiroaki Goto <1642211+StoneDot@users.noreply.github.com>
2019-02-11 00:27:55 +0900

    Rewrite tests using assert_contains

--- tests/macros/mod.rs
+++ tests/macros/mod.rs
@@ -23,3 +23,20 @@ macro_rules! test {
         }
     };
 }
+
+macro_rules! assert_contains {
+    ($haystack:expr, $needle:expr) => ({
+        match (&$haystack, &$needle) {
+            (haystack, needle) => {
+                if !haystack.contains(needle) {
+                    panic!(r#"assertion failed: `(haystack contains needle)`
+   needle: `{:?}`,
+ haystack: `{:?}`"#, needle, haystack)
+                }
+            }
+        }
+    });
+    ($haystack:expr, $needle:expr,) => ({
+        assert_contains!($haystack, $needle)
+    });
+}
