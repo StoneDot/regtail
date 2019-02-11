713da6d35e66d5576713a360a80b7626b6b975aa
Hiroaki Goto <1642211+StoneDot@users.noreply.github.com>
2019-02-11 15:51:20 +0900

    Add sleep duration

--- tests/single_file.rs
+++ tests/single_file.rs
@@ -27,7 +27,7 @@ use utils::KillStatus;
 mod macros;
 mod utils;
 
-const WAIT_TIME: Duration = Duration::from_millis(50);
+const WAIT_TIME: Duration = Duration::from_millis(200);
 
 test!(simple_run, |dir: WorkingDir, mut cmd: Command| {
     let mut child = RunningCommand::create(cmd.arg(dir.path_arg()).spawn().unwrap());
